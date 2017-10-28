7bfd3fadcf8ca52770667bac0de9840ec11f8291
Frederico Veloso <fportela@gmail.com>
2017-10-28 01:49:52 -0200

    Update todoListRoutes.js

--- todoListRoutes.js
+++ todoListRoutes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   var todoList = require('./controllers/todoListController');
 
   // todoList Routes
-  app.route('/atosFNDE').get(todoList.listAtos);
+  app.route('/getData').get(todoList.getData);
 
 };
 
